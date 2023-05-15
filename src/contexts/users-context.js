8d388b1ff64287bf17cc63d2582138d108fcd551
AgneBalc <agne.martinaityte@gmail.com>
2023-05-15 12:27:51 +0300

    users context created

---
+++ src/contexts/users-context.js
@@ -0,0 +1,52 @@
+import { createContext, useEffect, useReducer } from "react";
+
+const UsersContext = createContext();
+
+const USERS_ACTIONS = {
+  GET: 'get_all_users',
+  LOGIN: 'login_user',
+  LOGOUT: 'logout_user'
+}
+
+const usersReducer = (state, action) => {
+  switch (action.type) {
+    case USERS_ACTIONS.GET:
+      return { ...state, users: action.data };
+    case USERS_ACTIONS.LOGIN:
+      return { ...state, isLoggedIn: true };
+    case USERS_ACTIONS.LOGOUT:
+      return { ...state, isLoggedIn: false };
+    default:
+      return state;
+  }
+}
+
+const UsersProvider = ({ children }) => {
+  const [users, dispatchUsers] = useReducer(usersReducer, {
+    isLoggedIn: false,
+    users: [],
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:8080/users')
+      .then(res => res.json())
+      .then(data => dispatchUsers({
+        type: USERS_ACTIONS.GET,
+        data,
+      }))
+  }, []);
+
+  return (
+    <UsersContext.Provider
+      value={{
+        users,
+        dispatchUsers,
+      }}
+    >
+      {children}
+    </UsersContext.Provider>
+  );
+}
+
+export default UsersContext;
+export { UsersProvider, USERS_ACTIONS };
