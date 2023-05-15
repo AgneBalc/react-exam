ea19c433959c14fcc385b9573a678a59745d4ce4
AgneBalc <agne.martinaityte@gmail.com>
2023-05-15 15:48:09 +0300

    logout functionality added

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import StyledNavbar from "./navbar-styles";
 import { useContext } from "react";
-import UsersContext from "../../../contexts/users-context";
+import UsersContext, { USERS_ACTIONS } from "../../../contexts/users-context";
 
 const Navbar = () => {
-  const { users: { isLoggedIn } } = useContext(UsersContext);
+  const { users: { isLoggedIn }, dispatchUsers } = useContext(UsersContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatchUsers({ type: USERS_ACTIONS.LOGOUT });
+    navigate('/');
+  }
 
   return (
     <StyledNavbar>
@@ -12,7 +18,7 @@ const Navbar = () => {
         <ul>
           <li><NavLink to='/'>Home</NavLink></li>
           <li><NavLink to='/add'>Add Post</NavLink></li>
-          <li>Log Out</li>
+          <li onClick={handleLogout}>Log Out</li>
         </ul>
       ) : (
         <ul>
